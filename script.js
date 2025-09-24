101616ec4fd222a26dcdbf8cb87a0e37468ce228
Akash-1212579 <dalbhanjanakash1@gmail.com>
2025-09-24 23:33:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-function getFormvalue() {
-    //Write your code here
+let btn = document.getElementById("btn");
 
+function getFormvalue() {
+    let name = document.getElementById("uname").value;
+    let lastName = document.getElementById("ulname").value;
+
+    if (name && lastName) {
+        alert(`${name} ${lastName}`);
+    }
 }
+
+btn.addEventListener("click", getFormvalue);
